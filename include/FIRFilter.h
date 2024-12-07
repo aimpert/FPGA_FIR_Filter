bc67dff08eaaaaeb037b9bcbc02a8bd6f6c00464
aimpert <aleximpert@gmail.com>
2024-12-06 19:07:30 -0800

    set up for cosim debugging

--- include/FIRFilter.h
+++ include/FIRFilter.h
@@ -14,22 +14,6 @@ enum option {
 };
 
 
-float* convolution(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength);
+void convolution(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength, float *y);
 
 void writeData(option type, float* data, size_t length);
-
-// class FIRFilter {
-//     private:
-//         const float *x;
-//         unsigned long inputLength;
-//         const float *h;
-//         unsigned long filterLength;
-//         float *y;
-//         unsigned long outputLength;
-//     public:
-//     FIRFilter(const float *inputSignal, unsigned long inputLength, const float *filterSignal, unsigned long filterLength);
-//     //~FIRFilter();
-
-//     void applyFilter();
-//     float* getOutput();
-// };
--- src/FIRFilter.cpp
+++ src/FIRFilter.cpp
@@ -1,8 +1,15 @@
 #include "../include/FIRFilter.h"
 
+#define MAX_INPUT_LENGTH 1846172
+#define MAX_FILTER_LENGTH 11
 
-float* convolution(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength) {
-    float* y = new float[inputLength]();
+void convolution(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength, float *y) {
+    #pragma HLS INTERFACE m_axi port=x depth=MAX_INPUT_LENGTH
+    #pragma HLS INTERFACE m_axi port=h depth=128
+    #pragma HLS INTERFACE m_axi port=y depth=MAX_INPUT_LENGTH
+    #pragma HLS INTERFACE s_axilite port=inputLength
+    #pragma HLS INTERFACE s_axilite port=filterLength
+    #pragma HLS INTERFACE s_axilite port=return
     for (unsigned long n = 0; n < inputLength; ++n) {
         y[n] = 0;
         for (unsigned long k = 0; k < filterLength; ++k) {
@@ -11,7 +18,6 @@ float* convolution(const float *x, unsigned long inputLength, const float *h, un
             }
         }
     }
-    return y;
 }
 
 
