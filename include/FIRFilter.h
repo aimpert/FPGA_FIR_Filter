909b36c65748534109f06fef7d3a6a995de13561
aimpert <aleximpert@gmail.com>
2024-12-09 19:44:59 -0800

    huge refactor

--- include/FIRFilter.h
+++ include/FIRFilter.h
@@ -3,18 +3,13 @@
 #include <vector>
 #include <fstream>
 
-// const std::string ROOT_DIR = "../../../../../"; // Vitis HLS
-const std::string ROOT_DIR = "./"; // CMake
+const std::string ROOT_DIR = "../../../../../"; // Vitis HLS
+// const std::string ROOT_DIR = "./"; // CMake
 
-enum option {
-    input,
-    output,
-    output_gold,
-    all
-};
+const int N=11;
 
+typedef float coef_t;
+typedef float	data_t;
+typedef float	acc_t;
 
-void convolution(const float *x, const float *h, float *y);
-void convolution_sw(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength, float *y);
-
-void writeData(option type, float* data, size_t length);
+void fir(data_t *y, data_t x);
--- src/FIRFilter.cpp
+++ src/FIRFilter.cpp
@@ -1,69 +1,22 @@
 #include "../include/FIRFilter.h"
 
-#define MAX_INPUT_LENGTH 1846172
-#define MAX_FILTER_LENGTH 11
+void fir(data_t *y, data_t x) {
+    coef_t c[N] = {0.01444,0.03044,0.07242,0.12450,0.16675,0.18291,0.16675,0.12450,0.07242,0.03044,0.01444};
+    static data_t shift_reg[N];
+    acc_t acc;
+    int i;
 
-void convolution(const float *x, const float *h, float *y) {
-    #pragma HLS INTERFACE m_axi port=x depth=MAX_INPUT_LENGTH
-    #pragma HLS INTERFACE m_axi port=h depth=MAX_FILTER_LENGTH
-    #pragma HLS INTERFACE m_axi port=y depth=MAX_INPUT_LENGTH
-    #pragma HLS INTERFACE s_axilite port=return
-
-    #pragma HLS ARRAY_PARTITION variable=x complete
-    #pragma HLS ARRAY_PARTITION variable=h complete
-    #pragma HLS ARRAY_PARTITION variable=y complete
-
-    for (int n = 0; n < MAX_INPUT_LENGTH; ++n) {
-        #pragma HLS PIPELINE II=1
-        y[n] = 0;
-        for (int k = 0; k < MAX_FILTER_LENGTH; ++k) {
-        #pragma HLS UNROLL factor=2
-            int index = n - k;
-            if (index >= 0) {
-                y[n] += h[k] * x[index];
-            }
-        }
-    }
-}
-
-void convolution_sw(const float *x, unsigned long inputLength, const float *h, unsigned long filterLength, float *y) {
-
-    for (unsigned long n = 0; n < inputLength; ++n) {
-        y[n] = 0;
-        for (unsigned long k = 0; k <  filterLength; ++k) {
-            if (n >= k) { 
-                y[n] += h[k] * x[n - k];
-           }
-        }
-    }
-}
-
-
-// Function to write input and output datas to a .dat file for Vitis HLS
-void writeData(option type, float* data, size_t length) {
-    auto writeArrayToFile = [](const std::string& filename, const float* data, size_t length) {
-        std::ofstream myfile(filename);
-        if (!myfile.is_open()) {
-            std::cerr << "File failed to open." << std::endl;
-        }
+    acc = 0;
+    Shift_Accum_Loop:
+    for (i = N - 1; i >= 0; i--) {
+        if (i == 0) {
+            acc += x * c[0];
+            shift_reg[0] = x;
+        } 
         else {
-            for (int i = 0; i < length; i++) {
-                myfile << data[i] << std::endl;
-            }
-            myfile.close();
+            shift_reg[i] = shift_reg[i - 1];
+            acc += shift_reg[i] * c[i];
         }
-    };
-
-    if (type == option::input || type == option::all) {
-        writeArrayToFile(ROOT_DIR + "input.dat", data, length);
     }
-
-    if (type == option::output || type == option::all) {
-        writeArrayToFile(ROOT_DIR + "output.dat", data, length);
-    }
-
-    if (type == option::output_gold || type == option::all) {
-        writeArrayToFile(ROOT_DIR + "output.gold.dat", data, length);
-    }
-
+    *y = acc;
 }
